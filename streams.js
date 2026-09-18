d9649f6f84a566c26a65efe432a5ceb9127255de
Sachin Kumar <schnkumar762@gmail.com>
2026-09-18 14:02:20 +0530

    Refactor streams.js to implement readable stream functionality for largeFile.txt

--- streams.js
+++ streams.js
@@ -1,1 +1,15 @@
-const fs = require('fs');
+const fs = require("fs");
+
+const readableStream = fs.createReadStream("largeFile.txt", "utf8");
+
+readableStream.on("data", (chunk) => {
+  console.log("Received chunk", chunk);
+});
+
+readableStream.on("end", (chunk) => {
+  console.log("Finished reading file");
+});
+
+readableStream.on("error", (err) => {
+  console.error("Error reading file");
+});
