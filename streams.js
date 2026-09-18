c32d824c5503d1dea8694d11e5695481297096cf
Sachin Kumar <schnkumar762@gmail.com>
2026-09-18 14:52:02 +0530

    Refactor streams.js to uncomment and enable readable stream functionality

--- streams.js
+++ streams.js
@@ -1,19 +1,20 @@
-// const fs = require("fs");
+const fs = require("fs");
 
-// const readableStream = fs.createReadStream("largeFile.txt", "utf8");
+const readableStream = fs.createReadStream("largeFile.txt", "utf8");
 
-// readableStream.on("data", (chunk) => {
-//   console.log("Received chunk", chunk);
-// });
+readableStream.on("data", (chunk) => {
+  console.log("Received chunk", chunk);
+});
 
-// readableStream.on("end", (chunk) => {
-//   console.log("Finished reading file");
-// });
+readableStream.on("end", (chunk) => {
+  console.log("Finished reading file");
+});
 
-// readableStream.on("error", (err) => {
-//   console.error("Error reading file");
-// });
+readableStream.on("error", (err) => {
+  console.error("Error reading file");
+});
 
+/*
 const fs = require("fs");
 
 const file = fs.createWriteStream("./big.file");
@@ -23,3 +24,24 @@ for (let i = 0; i <= 1e6; i++) {
 }
 
 file.end();
+
+ */
+
+const { Transform } = require("stream");
+
+const upperCaseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().toUpperCase());
+    callback();
+  },
+});
+
+const writableStream = fs.createWriteStream("outFile.txt");
+
+writableStream.on("finish", () => {
+  console.log("finished writing files");
+});
+
+//concept of piping
+
+readableStream.pipe(upperCaseTransform).pipe(writableStream);
