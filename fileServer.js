16aa1dd4c893522394b01a75f2aea3873bbc1839
Sachin Kumar <schnkumar762@gmail.com>
2026-09-18 14:34:13 +0530

    Refactor fileServer.js to use stream for serving big.file

--- fileServer.js
+++ fileServer.js
@@ -3,10 +3,16 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  fs.readFile("./big.file", (err, data) => {
-    if (err) throw err;
-    res.end(data);
-  });
+  /* not using stream */
+  // fs.readFile("./big.file", (err, data) => {
+  //   if (err) throw err;
+  //   res.end(data);
+  // });
+
+  /* using stream */
+
+  const src = fs.createReadStream("./big.file");
+  src.pipe(res);
 });
 
 server.listen(8000);
