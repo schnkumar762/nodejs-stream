c22fe65de8d6db9fb7820f79355a0ba7fa755e3b
Sachin Kumar <schnkumar762@gmail.com>
2026-09-18 14:10:16 +0530

    Remove commented-out readable stream code and add big.file creation logic

--- streams.js
+++ streams.js
@@ -1,15 +1,25 @@
+// const fs = require("fs");
+
+// const readableStream = fs.createReadStream("largeFile.txt", "utf8");
+
+// readableStream.on("data", (chunk) => {
+//   console.log("Received chunk", chunk);
+// });
+
+// readableStream.on("end", (chunk) => {
+//   console.log("Finished reading file");
+// });
+
+// readableStream.on("error", (err) => {
+//   console.error("Error reading file");
+// });
+
 const fs = require("fs");
 
-const readableStream = fs.createReadStream("largeFile.txt", "utf8");
+const file = fs.createWriteStream("./big.file");
 
-readableStream.on("data", (chunk) => {
-  console.log("Received chunk", chunk);
-});
+for (let i = 0; i <= 1e6; i++) {
+  file.write("my name is Sachin Kumar");
+}
 
-readableStream.on("end", (chunk) => {
-  console.log("Finished reading file");
-});
-
-readableStream.on("error", (err) => {
-  console.error("Error reading file");
-});
+file.end();
